3f54b1ab41511b4cb2adc3e3b01906b96bf81a33
NurWedebrand <aysenurmercan98@gmail.com>
2025-06-03 19:25:25 -0500

    ProfilePageStepDefs.java updated

--- src/test/java/com/azulcrm/step_definitions/ProfilePageStepDefs.java
+++ src/test/java/com/azulcrm/step_definitions/ProfilePageStepDefs.java
@@ -1,5 +1,4 @@
 package com.azulcrm.step_definitions;
-
 import com.azulcrm.pages.LoginPage;
 import com.azulcrm.pages.ProfilePage;
 import com.azulcrm.utilities.BrowserUtils;
@@ -14,81 +13,43 @@ import org.junit.Assert;
 import java.util.List;
 
 public class ProfilePageStepDefs {
+
     LoginPage loginPage = new LoginPage();
     ProfilePage profilePage = new ProfilePage();
 
-    @Given("user logs in as hr user")
-    public void userLogsAsHrUser() {
-        Driver.getDriver().get("https://qa.azulcrm.com/");
-        loginPage.login(ConfigurationReader.getProperty("hr_username"), ConfigurationReader.getProperty("hr_password"));
-    }
-
-    @Given("user logs in as helpdesk user")
-    public void userLogsInAsHelpdeskUser() {
-        Driver.getDriver().get("https://qa.azulcrm.com/");
-        loginPage.login(ConfigurationReader.getProperty("helpdesk_username"), ConfigurationReader.getProperty("helpdesk_password"));
-    }
-
-    @Given("user logs in as marketing user")
-    public void userLogsInAsMarketingUser() {
-        Driver.getDriver().get("https://qa.azulcrm.com/");
-        loginPage.login(ConfigurationReader.getProperty("marketing_username"), ConfigurationReader.getProperty("marketing_password"));
-    }
-
-    @When("user clicks on My Profile option under the user menu.")
+    @When("user clicks on My Profile option under the User Menu")
     public void user_clicks_on_my_profile_option_under_the_user_menu() {
         profilePage.userMenu.click();
         BrowserUtils.waitFor(3);
         profilePage.myProfileItem.click();
         BrowserUtils.waitForVisibility(profilePage.generalItem, 10);
     }
-
-    @And("user sees his her hr email in the title.")
-    public void userSeesHisHerHrEmailInTheTitle() {
-        Assert.assertTrue(Driver.getDriver().getTitle().contains(ConfigurationReader.getProperty("hr_username")));
+    @And("user sees his her {string} email in the title")
+    public void userSeesHisHerEmailInTheTitle(String userType) {
+        if (userType.equalsIgnoreCase("hr")) {
+            Assert.assertTrue(Driver.getDriver().getTitle().contains(ConfigurationReader.getProperty("hr_username")));
+        } else if (userType.equalsIgnoreCase("helpdesk")) {
+            Assert.assertTrue(Driver.getDriver().getTitle().contains(ConfigurationReader.getProperty("helpdesk_username")));
+        } else if (userType.equalsIgnoreCase("marketing")) {
+            Assert.assertTrue(Driver.getDriver().getTitle().contains(ConfigurationReader.getProperty("marketing_username")));
+        }
     }
-
-    @And("user sees his her helpdesk email in the title.")
-    public void userSeesHisHerHelpdeskEmailInTheTitle() {
-        Assert.assertTrue(Driver.getDriver().getTitle().contains(ConfigurationReader.getProperty("helpdesk_username")));
-    }
-
-    @And("user sees his her marketing email in the title.")
-    public void userSeesHisHerMarketingEmailInTheTitle() {
-        Assert.assertTrue(Driver.getDriver().getTitle().contains(ConfigurationReader.getProperty("marketing_username")));
-    }
-
     @When("user views the following options on My Profile page")
     public void user_views_the_following_options_on_my_profile_page(List<String> expectedOptions) {
+        // Write code here that turns the phrase above into concrete actions
         BrowserUtils.waitFor(10);
         List<String> actualOptions = BrowserUtils.getElementsText(profilePage.profileMenuItems);
         Assert.assertEquals(expectedOptions, actualOptions);
     }
 
-    @Then("user sees his her hr email under the general tab")
-    public void userSeesHisHerHrEmailUnderTheGeneralTab() {
-        String actualEmail = profilePage.confirmationEmail.getText();
-        String expectedEmail = ConfigurationReader.getProperty("hr_username");
-        Assert.assertEquals(expectedEmail, actualEmail);
+    @Then("user sees his her {string} email under the general tab")
+    public void userSeesHisHerUnderTheGeneralTab(String userType) {
+        if (userType.equalsIgnoreCase("hr")) {
+            Assert.assertEquals(ConfigurationReader.getProperty("hr_username"), profilePage.confirmationEmail.getText());
+        } else if (userType.equalsIgnoreCase("helpdesk")) {
+            Assert.assertEquals(ConfigurationReader.getProperty("helpdesk_username"), profilePage.confirmationEmail.getText());
+        } else if (userType.equalsIgnoreCase("marketing")) {
+            Assert.assertEquals(ConfigurationReader.getProperty("marketing_username"), profilePage.confirmationEmail.getText());
+        }
     }
-
-    @Then("user sees his her helpdesk email under the general tab")
-    public void userSeesHisHerHelpdeskEmailUnderTheGeneralTab() {
-        String actualEmail = profilePage.confirmationEmail.getText();
-        String expectedEmail = ConfigurationReader.getProperty("helpdesk_username");
-        Assert.assertEquals(expectedEmail, actualEmail);
-    }
-
-    @Then("user sees his her marketing email under the general tab")
-    public void userSeesHisHerMarketingEmailUnderTheGeneralTab() {
-        String actualEmail = profilePage.confirmationEmail.getText();
-        String expectedEmail = ConfigurationReader.getProperty("marketing_username");
-        Assert.assertEquals(expectedEmail, actualEmail);
-    }
-    //TODO: Look for some alternatives--->Given user logs in as hr-helpdesk-marketing user steps all together
-    //TODO: Look for some alternatives--->And user sees his her hr-helpdesk-marketing email in the title steps all together
-    //TODO: Look for some alternatives--->Given user logs in as hr-helpdesk-marketing user steps all together
-
-
-
 }

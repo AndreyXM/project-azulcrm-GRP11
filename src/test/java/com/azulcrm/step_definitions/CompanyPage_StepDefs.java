1bb56907add348be7c9ad2953b82c92c618d4468
rjbelleza1 <ralphbelleza@yahoo.com>
2025-06-06 13:29:15 -0400

    created a method for enter username and password

--- src/test/java/com/azulcrm/step_definitions/CompanyPage_StepDefs.java
+++ src/test/java/com/azulcrm/step_definitions/CompanyPage_StepDefs.java
@@ -6,41 +6,30 @@ import com.azulcrm.pages.HomePage;
 import com.azulcrm.pages.LoginPage;
 import com.azulcrm.utilities.BrowserUtils;
 import com.azulcrm.utilities.ConfigurationReader;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.junit.Assert;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 public class CompanyPage_StepDefs {
 
-    @Given("User is logged in as a {string}")
-    public void user_is_logged_in_as_a(String userType) {
-        LoginPage loginPage = new LoginPage();
-        HomePage homePage = new HomePage();
+    LoginPage loginPage = new LoginPage();
 
-        homePage.logInLink.click();
-
-        BrowserUtils.waitFor(2);
-
-        if (userType.equalsIgnoreCase("hr1")) {
-            loginPage.login(ConfigurationReader.getProperty("hr_username"), ConfigurationReader.getProperty("hr_password"));
-        } else if (userType.equalsIgnoreCase("helpdesk1")) {
-            loginPage.login(ConfigurationReader.getProperty("helpdesk_username"), ConfigurationReader.getProperty("helpdesk_password"));
-        } else if (userType.equalsIgnoreCase("marketing1")) {
-            loginPage.login(ConfigurationReader.getProperty("marketing_username"), ConfigurationReader.getProperty("marketing_password"));
-        }
-
-    }
     @Then("User clicks Company under Activity Stream")
     public void user_clicks_company_under_activity_stream() {
         CompanyPage companyPage = new CompanyPage();
+        BrowserUtils.sleep(3);
         companyPage.companyLink.click();
     }
-        CompanyDashboardPage companyDashboardPage;
+    CompanyDashboardPage companyDashboardPage;
     @Then("Verify users see the following modules in the Company page shown as design")
     public void verify_users_see_the_following_modules_in_the_company_page_shown_as_design(List<String> expectedModuleNames) {
         companyDashboardPage = new CompanyDashboardPage();
+        BrowserUtils.sleep(3);
         List<String> actualModuleNames = companyDashboardPage.getModuleNames();
 
         Assert.assertEquals(expectedModuleNames, actualModuleNames);
@@ -48,4 +37,10 @@ public class CompanyPage_StepDefs {
 
     }
 
+
+    @Then("user enters username {string} and password {string}")
+    public void userEntersUsernameAndPassword(String username, String password) {
+        loginPage = new LoginPage();
+        loginPage.login(username,password);
+    }
 }
